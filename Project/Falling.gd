fd19844b02115087b33b80797b228a35bd399f9e
PizzaMozz <97705549+PizzaMozz@users.noreply.github.com>
2022-04-20 15:19:46 -0400

    Freeze Midair!!!!

--- Project/Falling.gd
+++ Project/Falling.gd
@@ -2,6 +2,7 @@ extends Node
 
 onready var SM = get_parent()
 onready var player = get_node("../..")
+onready var color = get_node_or_null("/root/Game/ColorRect")
 
 func _ready():
 	yield(player, "ready")
@@ -19,3 +20,7 @@ func physics_process(_delta):
 	var input_vector = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"),1.0)
 	player.velocity += player.move_speed * input_vector + player.gravity
 	player.move_and_slide(player.velocity, Vector2.UP)
+	if Input.is_action_just_pressed("mode_switch"):
+		color.visible = true
+		Global.mode = "chip"
+		SM.set_state("Block_move")
