123f69ea2c155681ddbd990fa1a0ddf6f5520c11
AspireToDev <97562355+AspireToDev@users.noreply.github.com>
2022-04-21 17:12:22 -0400

    :D

--- Project/StateMachine/Block_move.gd
+++ Project/StateMachine/Block_move.gd
@@ -6,10 +6,10 @@ onready var color = get_node_or_null("/root/Game/ColorRect")
 onready var label = get_node_or_null("/root/Game/Camera1/Chip_Count")
 onready var mode = Global.mode
 
-onready var right = get_node_or_null("Right")
-onready var left = get_node_or_null("Left")
-onready var up = get_node_or_null("Up")
-onready var down = get_node_or_null("Down")
+onready var right = player.get_node("Right")
+onready var left = player.get_node("Left")
+onready var up = player.get_node("Up")
+onready var down = player.get_node("Down")
 
 func _ready():
 	yield(player, "ready")
@@ -29,30 +29,22 @@ func physics_process(_delta):
 		else:
 			SM.set_state("Falling")
 	if Input.is_action_just_pressed("right"):
-		#if right != null:
-		#	right.position.x += 275
-		#	if right.is_not_colliding:
-		#		player.position.x += 275
-		#	else: 
-		#		right.position.x -= 275
-		#else: 
-		#	print("It is null")
-		if Global.chips > 0:
+		if Global.chips > 0 and not right.is_colliding():
 			player.position.x += 275
 			Global.chips -= 1
 			label.ready()
 	if Input.is_action_just_pressed("left"):
-		if Global.chips >0: 
+		if Global.chips >0 and not left.is_colliding(): 
 			player.position.x -= 275
 			Global.chips -= 1
 			label.ready()
 	if Input.is_action_just_pressed("jump"):
-		if Global.chips >0: 
+		if Global.chips >0 and not up.is_colliding(): 
 			player.position.y -= 275
 			Global.chips -= 1
 			label.ready()
 	if Input.is_action_just_pressed("down"):
-		if Global.chips > 0:
+		if Global.chips > 0 and not down.is_colliding():
 			player.position.y += 275
 			Global.chips -= 1
 			label.ready()
